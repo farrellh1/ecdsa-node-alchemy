8708cc8810afa11ee69283aa55bb630397440f1e
farrellh1 <farrell.hauzan@gmail.com>
2024-03-13 18:58:59 +0700

    Init project

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3042;
+
+app.use(cors());
+app.use(express.json());
+
+const balances = {
+  "b6a96a30859af0edb0b64b5f2d6fd0737568242d": 100,
+  "0f7aebd62459f3d2cfb2e8241eb1bf566469e8b5": 50,
+  "93ce2f4ba4d6d944d8853cecc4369a82ed103c3c": 75,
+};
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post("/send", (req, res) => {
+  const { sender, recipient, amount } = req.body;
+
+  setInitialBalance(sender);
+  setInitialBalance(recipient);
+
+  if (balances[sender] < amount) {
+    res.status(400).send({ message: "Not enough funds!" });
+  } else {
+    balances[sender] -= amount;
+    balances[recipient] += amount;
+    res.send({ balance: balances[sender] });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
